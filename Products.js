4e37bb3b7b7d97bbd5317e0ce94f8add2ecb7a3c
stlmargy <stellahmargy0211@gmail.com>
2025-03-24 12:38:47 +0300

    adds script tag to link js file and GET requests-fetch all products

--- Products.js
+++ Products.js
@@ -1,3 +1,15 @@
-const getProducts = () =>{
+const getProducts = () => {
+    fetch(`http://localhost:3000/items`)
+    .then((response)=>{
+        if(!response.ok) throw new Error(`HTTP Error: ${response.status}`);
+        return response.json()
+    })
+    .then((products)=>console.log(products))
+    .catch((error)=>console.log("error:" , error))
+  
     
-}
+};
+
+// INITIALIZE
+getProducts();
+
